a03e574bc8fab63213e66a67ec0de8beb49cf1b2
godwintest <etornamkofi5@gmail.com>
2023-09-06 04:52:11 +0100

    Deployment of Ai Image Generator App

---
+++ src/components/Myfooter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ImgGenerator/ImgGenerator'
+
+const Myfooter = () => {
+  return (
+    <div className="footer">
+        
+            <p>&copy; 2023  |  All Right Reserved  |</p>
+            <p className='abc'>Developed By <a href="https://linkedin.com/edorgodwin">Edor Godwin Etornam</a> </p>
+            
+        </div>
+  )
+}
+
+export default Myfooter
